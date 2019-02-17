8e7d3b800b7cf076016fcc3b0340d04f65d6ee48
Juan diego <diiegooliveros@gmail.com>
2019-02-17 18:41:36 -0500

    Commit inicial conel navbar y las rutas funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {SearchComponent} from './components/search/search.component';
+import {ArtistaComponent} from './components/artista/artista.component';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
 
-import { AppComponent } from './app.component';
+/* importamos las rutas y las declaramos en los imports con RouterModule.forRoot(ROUTES) */
+import {ROUTES} from './app.route';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
