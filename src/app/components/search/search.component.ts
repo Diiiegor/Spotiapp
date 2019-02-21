c1858c23dda0117e48d422d320d6ac48bdcb039b
Juan diego <diiegooliveros@gmail.com>
2019-02-20 22:17:44 -0500

    Se agrega funcionalidad al componenete seacrh y se crea funcion getArtista en el spotifyService que es la funcion encargada de hacer la peticion http para buscar artistas

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artistas: any[] = [];
+
+  constructor(private spotify: SpotifyService) {
+  }
 
   ngOnInit() {
+  }
+
+  buscar(term: string) {
+    this.spotify.getArtista(term).subscribe((data: any) => {
+      console.log(data.artists.items);
+      this.artistas = data.artists.items;
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,6 +10,7 @@ export class SpotifyService {
     console.log('Spotify service listo');
   }
 
+  /*trae los nuevos lansamientos del api de spotify*/
   getNewRelases() {
 
     const headers = new HttpHeaders({
@@ -17,5 +18,14 @@ export class SpotifyService {
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  }
+
+  /*busca un artista*/
+  getArtista(termino: string) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQDmM9vTGZ7hFI79MOdhTfqcZjXlvPhFna51oFGcupBm8ZESPLiurCVm6HpInV-OBhkL0uezHnPaBqAKyH0'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
   }
 }
